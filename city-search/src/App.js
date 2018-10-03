884587a053d21065679472fc60c4810c8e4755b1
Joey Ching <joeyching1@gmail.com>
2018-10-03 17:16:33 -0400

    added grouping by states and displaying city details

--- city-search/src/App.js
+++ city-search/src/App.js
@@ -3,40 +3,47 @@ import SearchField from './components/SearchField';
 import ResultBox from './components/ResultBox';
 import './App.css';
 
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       target: '',
-      results: []
+      search_str: '',
+      zipcodes: [],
+      states: [],
+      details: []
     };
   }
 
   getNewTarget = (newTarget) => {
-    let search_str = ''
+    let new_search_str = ''
     for (let i = 0; i < newTarget.length; i++) {
       if (newTarget.charAt(i) === ' ') {
-        search_str += '%20';
+        new_search_str += '%20';
       } else {
-        search_str += newTarget.toUpperCase().charAt(i);
+        new_search_str += newTarget.toUpperCase().charAt(i);
       }
     }
     this.setState({
-      target: search_str
+      target: newTarget.toUpperCase(),
+      search_str: new_search_str,
+      zipcodes: [],
+      states: [],
+      details: []
     }, this.getCities);
-    console.log(search_str);
+    console.log('search_str:' + new_search_str);
+    console.log('target:' + newTarget.toUpperCase());
   }
 
   getCities = () => {
     if (this.state.target.length > 0) {
-      this.callApi();
+      this.getZips();
     } else {
       console.log('Error: empty string');
     }
   }
 
-  callApi = async () => {
+  getZips = async () => {
     const url = 'http://ctp-zip-api.herokuapp.com/city/' + this.state.target;
     await fetch(url)
       .then(res => {
@@ -48,12 +55,45 @@ class App extends Component {
       }).then(data => {
         console.log(data);
         this.setState({
-          results: data
+          zipcodes: data
         });
+        return data
+      }).then(data => {
+        data.forEach(this.getCityDetails);
       }).catch(err => {
+        console.log(err);
+      });
+  }
+
+  getCityDetails = async (zipcode) => {
+    const url = 'http://ctp-zip-api.herokuapp.com/zip/' + zipcode;
+    await fetch(url)
+      .then(res => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          throw Error('No results');
+        }
+      }).then(data => {
+        let result = data.filter(entry => (entry.City === this.state.target && entry.ZipCodeType === "STANDARD"))[0];
+        if (!this.state.states.includes(result.State)) {
+          this.setState({
+            states: [...this.state.states, result.State]
+          });
+        }
+        let city_detail = {
+          "state": result.State,
+          "zipcode": result.Zipcode,
+          "city_name": result.LocationText,
+          "location": '(' + result.Lat + ',' + result.Long + ')',
+          "population": result.EstimatedPopulation ? result.EstimatedPopulation : "No data",
+          "total_wages": result.TotalWages ? result.TotalWages : "No data"
+        };
         this.setState({
-          results: []
+          details: [...this.state.details, city_detail]
         });
+        console.log(city_detail);
+      }).catch(err => {
         console.log(err);
       });
   }
@@ -66,7 +106,7 @@ class App extends Component {
         </div>
         <div className="App-body">
           <SearchField changeHandler={this.getNewTarget} />
-          <ResultBox results={this.state.results} />
+          <ResultBox results={this.state.details} states={this.state.states} />
         </div>
       </div>
     );
--- city-search/src/components/ResultBox.js
+++ city-search/src/components/ResultBox.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { Card, Collapsible, CollapsibleItem } from 'react-materialize';
+
 
 class Result extends Component {
+  render_collapsible = (city) => {
+    return (
+      <CollapsibleItem header={city.zipcode}>
+        <ul>
+          <li>City name: {city.city_name}</li>
+          <li>Location: {city.location}</li>
+          <li>Population (Estimated): {city.population}</li>
+          <li>Total wages: {city.total_wages}</li>
+        </ul>
+      </CollapsibleItem>
+    )
+  };
+
   render() {
     return (
-      <div className="card">
-        <h2 className="card-header">{this.props.zip}</h2>
-      </div>
+      <Card title={this.props.state_name}>
+        <Collapsible accordion>
+          {this.props.cities.map(this.render_collapsible)}
+        </Collapsible>
+      </Card>
     );
   }
 }
@@ -16,9 +33,13 @@ class ResultBox extends Component {
     if (this.props.results.length !== 0) {
       return (
         <div className="result-box">
-          {this.props.results.map((zip, i) => {
+          {this.props.states.map((state, i) => {
+            let cities = this.props.results.filter(entry => entry.state === state);
+            console.log('Now processing state: ' + state);
+            console.log('Passing these cities...');
+            console.log(cities);
             return (
-              <Result zip={zip} index={i} key={i} />
+              <Result state_name={state} cities={cities} index={i} key={i} />
             );
           })}
         </div>
